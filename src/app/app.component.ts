6b53d628f1e68783da269bd273b2c2afc3b4de77
Luis Nazario Loyola Aquino <lloyola@sunafil.gob.pe>
2023-01-25 16:52:31 -0500

    APP-emplados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de empleados';
+  title="aqui estoy soy yo"
+
+  empleados: Empleado [] = [
+    new Empleado("juan","Diaz","Presidente",7500),
+    new Empleado("Ana","Martin","Directora",5500),
+    new Empleado("Maria","Fdez","Jefa de Seccion",3500),
+    new Empleado("Laura","Lopez","Administrativo",2500),
+  ];
+
+  agregarEmpleado(){
+    let miEmpleado=new Empleado(this.cuadroNombre,this.cuadroapellido,this.cuadrocargo,this.cuadrosalario);
+    this.empleados.push(miEmpleado);
+  }
+
+  cuadroNombre:string="";
+  cuadronombre:string="";
+  cuadroapellido:string="";
+  cuadrocargo:string="";
+  cuadrosalario:number=0;  
+
 }
